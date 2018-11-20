1195a26fd9ac50eced13c01c8d32ff458986ca5f
goyurim <qpeijty1@gmail.com>
2018-11-20 19:29:33 +0900

    search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,11 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    if params[:search]
+      @posts = Post.search(params[:search])
+    else
+      @posts = Post.all
+    end
   end
 
   # GET /posts/1
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_many :likes
 	has_many :comments
+
+	def self.search(query)
+		self.where("title || content LIKE ?","%#{query}%")
+	end
 end
